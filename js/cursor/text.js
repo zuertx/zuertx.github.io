568c1ad5f109a0e36ae83e7b18a91dedd5690236
zuertx <2275688917@qq.com>
2020-06-27 16:11:50 +0800

    Site updated: 2020-06-27 16:11:50

--- js/cursor/text.js
+++ js/cursor/text.js
@@ -11,7 +11,7 @@ jQuery(document).ready(function($) {
         var x = e.pageX,
             y = e.pageY;
         $i.css({
-            "z-index": 99999999999999999999999999999999999999999999999999999999999999999999999999 ,
+            "z-index": 99999999999999 ,
             "top": y - 20,
             "left": x,
             "position": "absolute",
