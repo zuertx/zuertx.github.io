b1579556e2d0865f7c42a0c53549e36140ba6cfc
zuertx <2275688917@qq.com>
2020-06-27 14:21:40 +0800

    Site updated: 2020-06-27 14:21:39

---
+++ dist/music.js
@@ -0,0 +1,67 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+	  {
+        name: '心似烟火',
+        artist: '陈壹千',
+        url: 'http://music.163.com/song/media/outer/url?id=1399112638.mp3',
+        cover: 'http://p1.music.126.net/pE_xbRjFKTza8i7CKfmDuQ==/109951164447654929.jpg',
+      },
+      {
+        name: "Mine(Illenium Remix)",
+        artist: 'ILLENIUM/Phoebe Ryan',
+        url: 'http://music.163.com/song/media/outer/url?id=30780496.mp3',
+        cover: 'http://p1.music.126.net/94Zjhb6ibXN9jpIjRQDbUw==/7762552092459017.jpg',
+      },
+      {
+        name: 'You',
+        artist: 'Approaching Nirvana',
+        url: 'http://music.163.com/song/media/outer/url?id=3932159.mp3',
+        cover: 'http://p2.music.126.net/4d8hnmrMxDrKnn_0i1JqyA==/1728432278871211.jpg',
+      },
+	  {
+        name: '打上花火',
+        artist: 'DAOKO/米津玄師',
+        url: 'http://music.163.com/song/media/outer/url?id=496869422.mp3',
+        cover: 'http://p1.music.126.net/ZUCE_1Tl_hkbtamKmSNXEg==/109951163009282836.jpg',
+      },
+	  {
+        name: 'LOSER',
+        artist: '米津玄師',
+        url: 'http://music.163.com/song/media/outer/url?id=512359195.mp3',
+        cover: 'http://p1.music.126.net/A-VUmkAaUVQ88iXBrsbQ1A==/109951163561900568.jpg',
+      },
+	  {
+        name: 'Break Again(Original Mix)',
+        artist: 'Jack Howes',
+        url: 'http://music.163.com/song/media/outer/url?id=33891204.mp3',
+        cover: 'http://p2.music.126.net/6jt922VYf1X6T7tm3pSQng==/3331520232351413.jpg',
+      },
+	  {
+        name: 'Calling Out',
+        artist: '塩ノ谷早耶香',
+        url: 'http://music.163.com/song/media/outer/url?id=26123904.mp3',
+        cover: 'http://p2.music.126.net/0jkWCJtVcjjHvz85UXO-YQ==/2348556836953953.jpg',
+      },
+	  {
+        name: 'Remember The Name',
+        artist: 'Fort Minor/Styles of Beyond',
+        url: 'http://music.163.com/song/media/outer/url?id=1345018.mp3',
+        cover: 'http://p1.music.126.net/BgmLuvqG1OymH-Hb_z_9IA==/18269485207688439.jpg',
+      },
+	  {
+        name: 'aLIEz',
+        artist: 'mizuki/SawanoHiroyuki[nZk]',
+        url: 'http://music.163.com/song/media/outer/url?id=29307041.mp3',
+        cover: 'http://p1.music.126.net/zJbvnVJcsTALLn0oJwr_tw==/109951164120509979.jpg',
+      },
+	  {
+        name: '野狼disco',
+        artist: '宝石Gem',
+        url: 'http://music.163.com/song/media/outer/url?id=1357785909.mp3',
+        cover: 'http://p2.music.126.net/USRrIEfSNxJ4JnfKp4f6pA==/109951164470147956.jpg',
+      },
+    ]
+});
